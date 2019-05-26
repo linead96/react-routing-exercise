05ca708ca447e014f29d30edde561364c6eef4c6
Linead96 <danielgarboza@yahoo.com>
2019-05-26 17:51:43 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Courses from "./containers/Courses/Courses";
+import Users from "./containers/Users/Users";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header>
+            <ul>
+              <li>
+                <NavLink to="/courses">Courses</NavLink>
+              </li>
+              <li>
+                <NavLink to="/users">Users</NavLink>
+              </li>
+            </ul>
+          </header>
+          <Switch>
+            <Route path="/courses" component={Courses} />
+            <Route path="/users" component={Users} />
+            <Redirect from="/all-courses" to="/courses" />
+            <Route render={() => <h1>Not Found </h1>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Course/Course.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Course extends Component {
+  state = {
+    title: null
+  };
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      this.setState({
+        title: param[1]
+      });
+    }
+  }
+  componentDidUpdate() {
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      if (param[1] !== this.state.title) {
+        this.setState({
+          title: param[1]
+        });
+      }
+    }
+  }
+
+  render() {
+    const { title } = this.state;
+    const { id } = this.props.match.params;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>You selected the Course with ID: {id} </p>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Course);
